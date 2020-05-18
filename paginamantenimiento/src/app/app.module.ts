27fdf8b1f58a14095971f21db905611ddd0df93a
ChristianCasalAktios <65538901+ChristianCasalAktios@users.noreply.github.com>
2020-05-18 14:06:24 +0200

    Componente creado por CLI

--- paginamantenimiento/src/app/app.module.ts
+++ paginamantenimiento/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PaginaMantenimiento } from './components/pagina-mantenimiento/pagina-mantenimiento.component';
+import { PaginamantenimientoComponent } from './components/paginamantenimiento/paginamantenimiento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaMantenimiento
+    PaginamantenimientoComponent,
   ],
   imports: [
     BrowserModule
---
+++ paginamantenimiento/src/app/components/paginamantenimiento/paginamantenimiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paginamantenimiento',
+  templateUrl: './paginamantenimiento.component.html',
+  styleUrls: ['./paginamantenimiento.component.css']
+})
+export class PaginamantenimientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
