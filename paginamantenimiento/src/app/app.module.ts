6db22efd52c3b25d0e49637508708345b365dba3
ChristianCasalAktios <65538901+ChristianCasalAktios@users.noreply.github.com>
2020-05-19 08:06:08 +0200

    Cambios nombres

--- paginamantenimiento/src/app/app.module.ts
+++ paginamantenimiento/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PaginamantenimientoComponent } from './components/paginamantenimiento/paginamantenimiento.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { PaginamantenimientoComponent } from './components/paginamantenimiento/p
     PaginamantenimientoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- paginamantenimiento/src/app/components/paginamantenimiento/paginamantenimiento.component.ts
+++ paginamantenimiento/src/app/components/paginamantenimiento/paginamantenimiento.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paginamantenimiento.component.css']
 })
 export class PaginamantenimientoComponent implements OnInit {
+  public dateInit: Date;
+  public dateEnd: Date;
+  public title: string;
 
-  constructor() { }
+  constructor() {
+    this.dateInit = new Date('May 18, 2020 16:40:00');
+    this.dateEnd = new Date('May 18, 2020 19:30:00');
+    this.title = 'Página en mantenimiento';
+  }
 
   ngOnInit(): void {
   }
